d8a05e34b4adf9bfa3bba33fb62826b3311de449
smallDazza <smalldazza@gmail.com>
2024-06-06 20:00:21 +1000

    first commit on thursday class

---
+++ third_main.py
@@ -0,0 +1,59 @@
+
+a = 10
+# b = 15
+
+# result = a + b
+
+# print(f"the result of {a} plus {b} equals {result}")
+
+# today = "tuesday"
+
+# if today == "monday":
+#     print("set an alarm for 5am")
+# elif today == "tuesday":
+#     print("set an alarm for 7am")
+# elif today == "wednesday":
+#     print("set alarm for 8am")
+
+# temp = 36
+
+# if temp > 35:
+#     print("its hot outside")
+# elif temp < 15:
+#     print("its cold outside")
+# else:
+#     print("temp is just right")
+
+# pass property
+#does nothing , just passes..........(same as return in js)
+# if temp > 35:
+#     pass
+# elif temp < 15:
+#     print("its cold outside")
+# else:
+#     print("temp is just right")
+
+# Boolean  Operators
+# AND, OR
+
+# a = True
+# b = False
+# result = a and b
+# print(result)
+
+# result = a or b
+# print(result)
+
+# result = not b
+# print(result)
+
+age = 35
+has_permission = True
+
+if age >= 20:
+    if has_permission:
+        print("acces granted")
+    else:
+        print("access denied")
+else:
+    print("access denied")
