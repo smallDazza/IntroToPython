f01648edae47ed8a5537fe6895b9bbc089ec2812
smallDazza <smalldazza@gmail.com>
2024-06-08 13:01:32 +1000

    last sat clas

--- enumerate_func.py
+++ enumerate_func.py
@@ -1,4 +1,14 @@
 
 
-## Enumerate function
-#
+## enumerate() function = finds the index and value in a list
+
+animals = ["cat","dog","bird","frog","horse","fish"]
+
+target = "frog"
+
+for  index, animal in enumerate(animals):
+    #print( index, animal)
+    if animal == target:
+        print(index, animal)
+        break
+
